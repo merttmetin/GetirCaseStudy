fe8f0ccee673fb01dbc73beb73459b49cc79aeb0
Mert <mert@MacBook-Air-mac.local>
2020-09-27 00:48:57 +0300

    port change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const db = require("./helper/dbProc")();
 const mainRoutes = require("./routes/mainRoutes");
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 const errorHandler = require("./middlewear/errorHandling");
 
