2be0008a576e8766e237d6499026f8ea2485a061
Mert <mert@MacBook-Air-mac.local>
2020-09-27 09:45:29 +0300

    middlewear commit

---
+++ middlewear/checkParameter.js
@@ -0,0 +1,24 @@
+
+const error  = require("./errorHandling");
+
+
+//Check parameters in request body.(Is missing or undefined).
+
+module.exports.checkParameterIsMissing = (req,res,next)=>{
+   
+   const params = req.body;
+
+   if(Object.keys(req.body).length<4){
+      error.missingParameter(res);
+   }
+   else if(params.startDate == undefined 
+          ||params.endDate == undefined
+          ||params.minCount == undefined
+          ||params.maxCount  == undefined){
+         
+      error.missingParameter(res);
+   }
+   else{
+      next();
+   }
+}
